ebc5a0e8277bdbb498dbc47b02b35da054fc8e38
ahr <ahrasheed@gmail.com>
2026-06-08 13:00:17 +0200

    (chore) Removes todo from installation to point pixi install to main branch

--- stack/emos-cli/cmd/install.go
+++ stack/emos-cli/cmd/install.go
@@ -293,9 +293,7 @@ func installPixi() error {
 	ui.Header("CLONING EMOS WORKSPACE")
 	ui.Faint("Target: " + projectDir)
 	if err := ui.Spinner("Cloning EMOS repository...", func() error {
-		// TODO(test-only): clone feature/plugins until pixi-first-class is on
-		// main. REVERT this --branch arg before merging.
-		c := exec.Command("git", "clone", "--depth", "1", "--branch", "feature/plugins", config.RepoURL(), projectDir)
+		c := exec.Command("git", "clone", "--depth", "1", config.RepoURL(), projectDir)
 		if out, err := c.CombinedOutput(); err != nil {
 			return fmt.Errorf("%s", strings.TrimSpace(string(out)))
 		}
